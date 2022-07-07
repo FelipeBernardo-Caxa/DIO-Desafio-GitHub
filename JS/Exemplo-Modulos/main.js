4ceb5acebfb96cec7634113d80d3f4c76b9dd082
FelipeBernardo-Caxa <felipebernardo12@hotmail.com>
2022-07-06 22:04:27 -0300

    Inclusão do conteúdo adquirido em JavaScript e do exemplo realizado em aula

---
+++ JS/Exemplo-Modulos/main.js
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Felipe" , 32));
+console.log(mostraCidade("Felipe", "Limeira-SP"));
+console.log(mostraHobby("Felipe" , "Musculação"));
